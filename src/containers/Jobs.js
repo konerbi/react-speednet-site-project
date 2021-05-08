f9cb523ca67ce98dfd2987bdbb1ab3e1d02a7ca6
Konrad Erbetowski <konrad.erbetowski@speednet.pl>
2021-05-08 12:45:55 +0200

    -- Jobs page

--- src/containers/Jobs.js
+++ src/containers/Jobs.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const Jobs = () => {
+	const [t, i18n] = useTranslation('common');
+
 	return (
 		<div>
-			JobOffers
+			<header className="header-intro-secondary">
+				<div className="container">
+					<h1>{t('jobs.header')}</h1>
+					<p>{t('jobs.subHeader')}</p>
+				</div>
+				<img src="src/assets/images/bg/bg_section_top_career.jpg" className="header-background"/>
+			</header>
+			<section className="section section-intro no-padd-bottom">
+				<div className="container">
+					<div className="col-sm-10 col-sm-offset-1">
+						<h3></h3>
+						<h3>{t('jobs.description_1')}</h3>
+						<p>&nbsp;</p>
+						<h3>{t('jobs.description_2')}</h3>
+						<p>&nbsp;</p>
+						<h3>{t('jobs.description_3')}
+							<a title="" href="mailto:praca@speednet.pl">praca@speednet.pl</a>.
+						</h3>
+					</div>
+				</div>
+			</section>
 		</div>
 	);
 };
