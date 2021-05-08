b8446374fdefa9181593e9b7cb601cab03b0e5f7
Konrad Erbetowski <konrad.erbetowski@speednet.pl>
2021-05-08 13:03:03 +0200

    -- NotFound page

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import {useTranslation} from "react-i18next";
 
 const NotFound = () => {
+	const [t, i18n] = useTranslation('common');
+
 	return (
 		<div>
-			NotFound
+			<header className="header-intro-secondary">
+				<div className="container">
+					<h1>{t('notFound.header')}</h1>
+					<p>{t('notFound.subHeader')}</p>
+				</div>
+			</header>
 		</div>
 	);
 };
